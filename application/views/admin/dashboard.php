4b524fbb3b6a57ac0aa5d89cbfb8f2fd03ba5d74
irhamrizaldy <43570194+irhamrizaldy@users.noreply.github.com>
2022-01-20 13:03:10 +0700

    div clickable -mobile version

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -21,16 +21,18 @@
         foreach ($tbl_admin as $total) {
     ?>
             <div class="col-12 col-sm-6 col-md-3">
-                <div class="info-box mb-3">
-                    <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-thumbs-up"></i></span>
+                <a style="color: black;" href="<?= base_url() ?>admin/admin">
+                    <div class="info-box mb-3">
+                        <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-thumbs-up"></i></span>
 
-                    <div class="info-box-content">
-                        <span class="info-box-text">Admin</span>
-                        <span class="info-box-number"><?php echo $total->total ?></span>
+                        <div class="info-box-content">
+                            <span class="info-box-text">Admin</span>
+                            <span class="info-box-number"><?php echo $total->total ?></span>
+                        </div>
+                        <!-- /.info-box-content -->
                     </div>
-                    <!-- /.info-box-content -->
-                </div>
-                <!-- /.info-box -->
+                    <!-- /.info-box -->
+                </a>
             </div>
     <?php
         }
@@ -46,15 +48,17 @@
             <div class="clearfix hidden-md-up"></div>
 
             <div class="col-12 col-sm-6 col-md-3">
-                <div class="info-box mb-3">
-                    <span class="info-box-icon bg-success elevation-1"><i class="fas fa-shopping-cart"></i></span>
+                <a style="color: black;" href="<?= base_url() ?>admin/item">
+                    <div class="info-box mb-3">
+                        <span class="info-box-icon bg-success elevation-1"><i class="fas fa-shopping-cart"></i></span>
 
-                    <div class="info-box-content">
-                        <span class="info-box-text">Goods</span>
-                        <span class="info-box-number"><?php echo $total->total ?></span>
+                        <div class="info-box-content">
+                            <span class="info-box-text">Goods</span>
+                            <span class="info-box-number"><?php echo $total->total ?></span>
+                        </div>
+                        <!-- /.info-box-content -->
                     </div>
-                    <!-- /.info-box-content -->
-                </div>
+                </a>
                 <!-- /.info-box -->
             </div>
     <?php
